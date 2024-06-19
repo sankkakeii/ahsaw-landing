900f480044ba1ed03cdf3980e078419ae34186c7
David Ikenna <sankkakeii@gmail.com>
2024-06-19 17:48:32 +0100

    add subsription section to donations modal

--- components/landing/Roadmap.jsx
+++ components/landing/Roadmap.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 
 export default function Roadmap() {
-    // const roadmapData = [
-    //     { milestone: 'Rescue 50 Dogs', timeline: 'Q1 2024', details: ['Define project scope', 'Identify stakeholders', 'Establish project team'] },
-    //     { milestone: 'Research and Planning', timeline: 'Q2 2024', details: ['Conduct market research', 'Develop project plan', 'Set budget and timeline'] },
-    //     { milestone: 'Development Phase', timeline: 'Q3-Q4 2024', details: ['Start development', 'Perform initial testing', 'Implement feedback'] },
-    //     { milestone: 'Testing and Refinement', timeline: 'Q1 2025', details: ['Conduct comprehensive testing', 'Refine product based on feedback', 'Prepare for launch'] },
-    //     { milestone: 'Launch', timeline: 'Q2 2025', details: ['Final preparations', 'Marketing and promotion', 'Official product launch'] },
-    //     { milestone: 'Post-Launch', timeline: 'Q3-Q4 2025', details: ['Monitor product performance', 'Gather user feedback', 'Plan for updates and improvements'] },
-    // ];
-
     const roadmapData = [
         {
             milestone: 'Rescue 50 Dogs',
@@ -107,7 +98,7 @@ export default function Roadmap() {
             <h1 className="text-4xl md:text-5xl font-bold leading-tight mb-10">Milestones</h1>
             <div className="relative w-full max-w-4xl">
                 {/* Central line */}
-                <div className="absolute left-1/2 transform -translate-x-1/2 h-full border-l-4 border-gray-300 hidden sm:block sm:z-50"></div>
+                <div className="absolute left-1/2 transform -translate-x-1/2 h-full border-l-4 border-gray-300 hidden sm:block"></div>
 
                 {roadmapData.map((item, index) => (
                     <div key={index} className={`flex flex-col md:flex-row ${index % 2 === 0 ? 'md:flex-row-reverse' : ''} items-center mb-10`}>
